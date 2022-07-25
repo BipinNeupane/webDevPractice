55ef36efdd6ada0e1100968d02dd3f62dc33cefa
BipinNeupane <bipin2022121@nami.edu.np>
2022-07-25 21:54:20 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+document.getElementById("p1").innerHTML = "Enter your username: ";
+
+let textValue;
+
+document.getElementById("myButton").onclick = function changeNames(){
+     textValue = document.getElementById("myInput").value;
+     document.getElementById("p1").innerHTML = "Welcome Back! " + textValue;
+}
+
+
+
+
