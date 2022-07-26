20c9e2f7b5ae1805428a5a88ff6f3b74e0335da6
BipinNeupane <bipin2022121@nami.edu.np>
2022-07-26 14:46:32 +0545

    fixed the html tag inputb

--- rightAngle.js
+++ rightAngle.js
@@ -5,8 +5,11 @@ let b;
 document.getElementById("myButton").onclick = function calculate(){
     // taking value from user and storing to a,b,c
     a = document.getElementById("inputa").value;
+    // converting string a into number and same for all sides
+    a = Number(a);
     b = document.getElementById("inputb").value;
+    b = Number(b);
     // c = underRoot(a^2+b^2)
     c = Math.sqrt(Math.pow(a,2) + Math.pow(b,2));
-    document.getElementById("update").innerHTML = "The value of c is: " + c;
-}
+    document.getElementById("update").innerText = "The value of c is: " + c;
+};
