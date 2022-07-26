70b271fdc912d655d99ba29b7864330c09d2dd62
BipinNeupane <bipin2022121@nami.edu.np>
2022-07-26 15:25:47 +0545

    added reset and decrease

--- counter.js
+++ counter.js
@@ -5,3 +5,16 @@ document.getElementById("increase").onclick = function increaseCount(){
     count += 1
     document.getElementById("count").innerHTML = count;
 }
+
+// creating a function when button reset is clicked
+document.getElementById("reset").onclick = function resetCount(){
+    count = 0
+    document.getElementById("count").innerHTML = count;
+}
+
+// creating a function when button decrease is clicked
+// count -= 1 cause we have to do -1 everytime
+document.getElementById("decrease").onclick = function decreaseCount(){
+    count -= 1
+    document.getElementById("count").innerHTML = count;
+}
