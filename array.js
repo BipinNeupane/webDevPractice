ab222367678438c6190ca72c6d91ed9fb2f18b77
BipinNeupane <bipin2022121@nami.edu.np>
2022-07-30 17:16:07 +0545

    learned many features and methods

---
+++ array.js
@@ -0,0 +1,24 @@
+let phoneModels = ["iPhone X", "iPhone XR", "iPhone 11", "iPhone 11 Pro"];
+
+// document.getElementById("changeMe").innerHTML = phoneModels + " ";
+
+// print all array elements
+// for (let phone of phoneModels){
+//     console.log(phone);
+// };
+
+
+// reverse an array
+
+// let myname = "bipin";
+// let nstr = ""
+// for (let i = phoneModels.length-1; i>=0;i--){
+//     nstr += phoneModels[i] + " ";
+// }
+// console.log(nstr);
+
+// remove custom string
+
+// let iwantto = "iPhone XR"
+// console.log(phoneModels.splice(phoneModels.indexOf(iwantto),1))
+// console.log(phoneModels);   
