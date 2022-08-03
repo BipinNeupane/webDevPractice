f16269a524c274afd06ed2862e4743601415d6d8
BipinNeupane <bipin2022121@nami.edu.np>
2022-08-03 17:04:52 +0545

    created a button which displays all lines

--- canvasPractice.js
+++ canvasPractice.js
@@ -1,14 +1,20 @@
 let canvas = document.querySelector("#myCanvas");
 let context = canvas.getContext("2d");
+let button = document.getElementById("myButton");
 
 // create a X in context
-context.beginPath();
-context.moveTo(0,0);
-context.lineTo(500,500);
-context.moveTo(500,0);
-context.lineTo(0,500);
-context.moveTo(250,0);
-context.lineTo(250,500);
-context.moveTo(0,250);
-context.lineTo(500,250); 
-context.stroke();
+
+
+button.onclick = () => {
+  context.beginPath();
+  context.moveTo(0, 0);
+  context.lineTo(500, 500);
+  context.moveTo(500, 0);
+  context.lineTo(0, 500);
+  context.moveTo(250, 0);
+  context.lineTo(250, 500);
+  context.moveTo(0, 250);
+  context.lineTo(500, 250);
+  context.lineWidth = 5;
+  context.stroke();
+};
